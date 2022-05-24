a91a374591c6f38cb2fd9f077d31d0d395fea3b3
Rohit Verma <verma.rohit.in@gmail.com>
2022-05-24 11:19:30 +0530

    feature: install react, mui

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+        <Button variant='contained'>Hello</Button>
+
+    </div>
+  );
+}
+
+export default App;
