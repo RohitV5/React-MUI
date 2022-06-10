8f730129e4c6e50071e867b216c4931109785116
Rohit Verma <verma.rohit.in@gmail.com>
2022-06-10 12:26:24 +0530

    feature:react-router-dom, signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import "./App.css";
-import { Button, CssBaseline, ThemeProvider, Typography } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
 import { theme } from "./themes/Theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./pages/auth/Signup";
+import Landing from "./pages/auth/Landing";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline>
-        <Button variant="contained">Hello</Button>
-        <Typography variant="h2">Hi I am typography</Typography>
-      </CssBaseline>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="signup" element={<SignUp />} />
+          {/* <Route path="invoices" element={<Invoices />} /> */}
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
